4a96d3cbee6f754693c7d1b7406e9289826a1e70
Arras <MArras@loanpacific.com>
2017-05-08 10:19:04 -0500

    initial commit

---
+++ home-alternate.php
@@ -0,0 +1,62 @@
+<?php
+/*
+Template Name: Home Alternate
+*/
+?>
+
+<?php get_header('header3'); ?>
+
+
+
+<div class="home-alternate-container" style="">
+	<div class="notes-container" style="height:600px; z-index:10; width:85%; display:inline-block; margin-top:15%; text-align:center;">
+	    <img src="http://blog.pacificunionfinancial.com/wp-content/uploads/2016/03/Mortgage-Notes-2.png">
+
+		<p class="notes-title">Mortgage Notes</p>
+
+		<p style="font-family: 'Open Sans', sans-serif;font-size:18px;color:#fff;">News of note and views with value for everyone from borrowers to professionals.</p>
+
+		<br />
+
+		<div style="text-align:center;">
+
+			<div class="notes house-note" style="">
+				<div style="float:none;height:17px;background-color:#f1f2f2;"></div>
+				<div class="notes-content" style="">
+					<p class="notes-sub-title" style="">HOUSE NOTES</p>
+					<p style="color:#686868;font-size:14px;">Buying or refinancing?<br />Here’s extra info for borrowers.</p>
+					<a href="http://blog.pacificunionfinancial.com/category/house-notes/" style="color:#1564a4;font-weight:700;text-decoration:none;font-size:14px;">READ MORE</a>
+				</div>
+			</div>
+			<div class="notes company-note" style="">
+				<div class="cut-out-arrow" style=""></div>
+				<div class="notes-content" style="">
+					<p class="notes-sub-title" style="">COMPANY NOTES</p>
+					<p style="color:#686868;font-size:14px;">Peek inside <br />the Pacific Union newsroom.</p>
+					<a href="http://blog.pacificunionfinancial.com/category/company-notes/" style="color:#1564a4;font-weight:700;text-decoration:none;font-size:14px;">READ MORE</a>
+				</div>
+			</div>
+			<div class="notes key-note" style="">
+				<div style="float:none;height:17px;background-color:#f1f2f2;"></div>
+				<div class="notes-content" style="">
+					<p class="notes-sub-title" style="">INDUSTRY NOTES</p>
+					<p style="color:#686868;font-size:14px;">Insight and intelligence <br /> for mortgage pros.</p>
+					<a href="http://blog.pacificunionfinancial.com/category/industry-notes/" style="color:#1564a4;font-weight:700;text-decoration:none;font-size:14px;">READ MORE</a>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+</div>
+	
+<style>
+@media only screen and (max-width: 960px){
+	#site-navigation ul a {
+	    color: #ffffff;
+	}
+}
+</style>
+
+<?php get_footer(); ?>
+
